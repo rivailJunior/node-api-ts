25113f44294c498cd026cb7647b5c88e3df3c1b8
rivailtarkuss <rivail@tarkuss.com.br>
2021-05-05 19:30:37 -0400

    [ADD] - Headers authorization on request

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -35,7 +35,11 @@ export class StormGlass {
     constructor(protected request: AxiosStatic) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-        const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${ this.stormGlassAPIPrams }&source=${ this.stormGlassAPISource }&end=1592113802&lat=${ lat }&lng=${ lng }`)
+        const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${ this.stormGlassAPIPrams }&source=${ this.stormGlassAPISource }&end=1592113802&lat=${ lat }&lng=${ lng }`, {
+            headers: {
+                Authorization: process.env.STORM_GLASS_TOKEN
+            }
+        })
         return this.normalizeResponse(response.data)
     }
     private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
