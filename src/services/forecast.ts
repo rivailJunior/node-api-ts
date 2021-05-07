5a9a8894192098c936160a1ae493e2c510ff2df8
rivailtarkuss <rivail@tarkuss.com.br>
2021-05-07 00:01:54 -0400

    [ADD] - Forecast service

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -14,14 +14,17 @@ export interface Beach {
     user: string
 }
 
-export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint { }
 
+export interface TimeForecast {
+    time: string;
+    forecast: BeachForecast[]
 }
 
 export class Forecast {
     constructor(protected stormGlass = new StormGlass()) { }
 
-    public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+    public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
         const pointsWithCorrectSources: BeachForecast[] = [];
         for (const beach of beaches) {
             const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -37,6 +40,22 @@ export class Forecast {
             }));
             pointsWithCorrectSources.push(...enricheBeachData);
         }
-        return pointsWithCorrectSources;
+        return this.mapForecastByTime(pointsWithCorrectSources);
+    }
+
+    private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+        const forecastByTime: TimeForecast[] = [];
+        for (const point of forecast) {
+            const timePoint = forecastByTime.find((f) => f.time === point.time);
+            if (timePoint) {
+                timePoint.forecast.push(point)
+            } else {
+                forecastByTime.push({
+                    time: point.time,
+                    forecast: [point]
+                })
+            }
+        }
+        return forecastByTime
     }
 }
